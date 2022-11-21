efbea36a66b7fd9063afa479938e7c1964792712
Jason Boyll <jason.boyll1@accesscfa.com>
2022-11-20 22:15:45 -0500

    CSV processing example

---
+++ csv/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    // example program processing csv data
+    let data = "\
+    name,age
+    Charles,43
+    Jackson,22
+    Jimmy,30
+    The dude, 46
+    ";
+
+    let records = data.lines();
+
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 {
+            continue;
+        }
+        let fields: Vec<_> = record
+            .split(',')
+            .map(|field| field.trim())
+            .collect();
+        if cfg!(debug_assertions) {
+            eprintln!("debug: {:?} -> {:?}", record, fields);
+        }
+        let name = fields[0];
+        if let Ok(age) =fields[1].parse::<f32>() {
+            println!("{}, {}", name, age);
+        }
+    }
+}
