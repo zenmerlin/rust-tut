b43a07957613b8df7f5de316b978508d971f273e
Jason Boyll <jason.boyll1@accesscfa.com>
2022-11-21 21:10:41 -0500

    Use string slices

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,16 +1,41 @@
 fn main() {
     let s = String::from("hello there!");
     println!("first word of s is {}", first_word(&s));
+    println!("second word of s is {}", second_word(&s));
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    let mut count = 0;
+    let mut start = 0;
+    let mut end = 0;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            count += 1;
+            if count == 1 {
+                start = i+1;
+            } else if count == 2 {
+                end = i;
+            }
+        }
+    }
+
+    if end < start {
+        end = s.len();
+    }
+
+    &s[start..end]
 }
