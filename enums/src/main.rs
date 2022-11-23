057692d71652f0e3496c3d7af4293ce071708ac0
Jason Boyll <jason.boyll1@accesscfa.com>
2022-11-22 21:28:31 -0500

    States

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -12,11 +12,18 @@ impl Message {
     }
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // etc
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
@@ -44,6 +51,7 @@ fn main() {
     }
 
     println!("Value of some coin: {}", value_in_cents(Coin::Penny));
+    println!("Value of some coin: {}", value_in_cents(Coin::Quarter(UsState::Alabama)));
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -54,6 +62,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
