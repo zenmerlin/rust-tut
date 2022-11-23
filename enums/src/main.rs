236becdb6d3e16595a7c0f06c65e3a0df3a5d5ad
Jason Boyll <jason.boyll1@accesscfa.com>
2022-11-22 21:32:42 -0500

    More matching options

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -52,6 +52,20 @@ fn main() {
 
     println!("Value of some coin: {}", value_in_cents(Coin::Penny));
     println!("Value of some coin: {}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    
+    match six { 
+        Some(x) => println!("Some number: {}", x),
+        None => println!("No number"),
+    }
+
+    match none {
+        Some(x) => println!("Some number: {}", x),
+        None => println!("No number"),
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -66,5 +80,12 @@ fn value_in_cents(coin: Coin) -> u8 {
             println!("State quarter from {:?}!", state);
             25
         }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
