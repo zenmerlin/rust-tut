85c0339a515b326e8ec8b57c964bc9efdcd05883
Jason Boyll <jason.boyll@gmail.com>
2022-11-26 21:56:18 -0500

    Implementation

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -20,12 +20,23 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+    results
 }
+
 
 #[cfg(test)]
 mod tests {
