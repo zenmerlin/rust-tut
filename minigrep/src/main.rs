41f6db16adaefff2cb8f53d9d3f6792932549fbd
Jason Boyll <jason.boyll@gmail.com>
2022-11-26 21:17:19 -0500

    Extract run logic from main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -13,12 +13,15 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
-
 struct Config {
     query: String,
     file_path: String,
