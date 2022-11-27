0934249619b12e2ebc04690559dab65cd88ded73
Jason Boyll <jason.boyll@gmail.com>
2022-11-27 10:58:50 -0500

    Iterator example calling next

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -7,6 +7,19 @@ fn iter_test_1() {
 
 }
 
+fn iter_test_2() {
+    let v1 = vec![1, 2, 3];
+
+    // Iterators need to be mutable because next() changes internal state
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
+
 fn main() {
    iter_test_1(); 
+   iter_test_2(); 
 }
