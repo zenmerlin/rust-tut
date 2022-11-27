8dc386f80fff7db85b7fd24337fc4275bad2972e
Jason Boyll <jason.boyll@gmail.com>
2022-11-27 11:11:26 -0500

    Refactor to lib

---
+++ iterators/src/lib.rs
@@ -0,0 +1,36 @@
+pub fn iter_test_1() {
+    let v1 = vec![1, 2, 3];
+
+    for val in v1.iter() {
+        println!("test1: got {val}");
+    }
+
+}
+
+pub fn iter_test_2() {
+    let v1 = vec![1, 2, 3];
+
+    // Iterators need to be mutable because next() changes internal state
+    let mut v1_iter = v1.iter();
+
+    // next() returns immutable references to values in the vector
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
+
+pub fn iter_consumable_test() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    let total: i32 = v1_iter.sum();
+    assert_eq!(total, 6);
+}
+
+pub fn collecting_iter() {
+    let v1 = vec![1, 2, 3];
+
+    // Calling map produces another iterator that is then processed with collect()
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
+}
--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,43 +1,7 @@
-fn iter_test_1() {
-    let v1 = vec![1, 2, 3];
-
-    for val in v1.iter() {
-        println!("test1: got {val}");
-    }
-
-}
-
-fn iter_test_2() {
-    let v1 = vec![1, 2, 3];
-
-    // Iterators need to be mutable because next() changes internal state
-    let mut v1_iter = v1.iter();
-
-    // next() returns immutable references to values in the vector
-    assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None);
-}
-
-fn iter_consumable_test() {
-    let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
-    let total: i32 = v1_iter.sum();
-    assert_eq!(total, 6);
-}
-
-fn collecting_iter() {
-    let v1 = vec![1, 2, 3];
-
-    // Calling map produces another iterator that is then processed with collect()
-    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
-    assert_eq!(v2, vec![2, 3, 4]);
-}
 
 fn main() {
-   iter_test_1(); 
-   iter_test_2(); 
-   iter_consumable_test();
-   collecting_iter();
+   iterators::iter_test_1(); 
+   iterators::iter_test_2(); 
+   iterators::iter_consumable_test();
+   iterators::collecting_iter();
 }
