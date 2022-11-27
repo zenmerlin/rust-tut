842bdcf28954537fc39fc91dbe58b936e6176c5a
Jason Boyll <jason.boyll@gmail.com>
2022-11-26 21:06:00 -0500

    Struct config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,20 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_configs(&args);
+    let config = parse_configs(&args);
 
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
 
-fn parse_configs(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_configs(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
 }
