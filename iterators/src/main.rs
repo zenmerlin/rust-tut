d20c25efaa8a08176b2842eb89326aa1b4094264
Jason Boyll <jason.boyll@gmail.com>
2022-11-27 11:07:15 -0500

    Collecting iterators

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -27,8 +27,17 @@ fn iter_consumable_test() {
     assert_eq!(total, 6);
 }
 
+fn collecting_iter() {
+    let v1 = vec![1, 2, 3];
+
+    // Calling map produces another iterator that is then processed with collect()
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
+}
+
 fn main() {
    iter_test_1(); 
    iter_test_2(); 
    iter_consumable_test();
+   collecting_iter();
 }
