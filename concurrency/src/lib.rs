570c0607082cbd34d1098e76d326630611fc34be
Jason Boyll <jason.boyll@gmail.com>
2022-11-27 20:56:05 -0500

    Wait for thread example

--- concurrency/src/lib.rs
+++ concurrency/src/lib.rs
@@ -17,6 +17,22 @@ pub fn example_thread_spawn() {
     handle.join().unwrap();
 }
 
+pub fn example_wait_for_thread() {
+    let handle = thread::spawn(|| {
+        for i in 1..=10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..=5 {
+        println!("hi number {} from main thread!", i);        
+        thread::sleep(Duration::from_millis(1));
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,5 +40,10 @@ mod tests {
     #[test]
     fn run_example_thread_spawn() {
         example_thread_spawn();
+    }
+
+    #[test]
+    fn run_example_wait_for_thread() {
+        example_wait_for_thread();
     }
 }
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
     concurrency::example_thread_spawn();
+    concurrency::example_wait_for_thread();
 }
